e14527f61063346fbea637778df66629402da343
harytakumi <hary.taku@gmail.com>
2026-09-17 14:46:16 +0700

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,11 @@
+links: {
+  email: "hary007@komdigi.go.id",
+  whatsapp: "+62 81380591386",
+  linkedin: "https://linkedin.com/in/harytakumi",
+  scholar: "",
+  orcid: "",
+  researchgate: "",
+  youtube: "",
+  instagram: "harytakumi",
+  cv: "assets/cv-hary-yudhanto.pdf"
+},
